9c32f08bc3906a3d71adaf9fa52d127142851285
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-07-03 18:29:05 +0700

    x

---
+++ helium/ins-helium.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# =========================================
+# Quick Setup | Script Setup Manager
+# Edition : Stable Edition V2.0
+# Auther  : givpn
+# (C) Copyright 2023
+# =========================================
+clear
+if [[ -e /usr/bin/helium ]]; then
+     echo ""
+     echo -e "${green}Ads Block Already Install${NC}"
+     echo ""
+	 read -n1 -r -p "Press any key to continue..."
+	 m-system
+else
+rm -rf /usr/bin/m-system
+wget -q -O /usr/bin/m-system https://raw.githubusercontent.com/givpn/AutoScriptXray/master/helium/menu/m-system.sh
+chmod +x /usr/bin/m-system
+rm -rf /usr/bin/helium
+wget -q -O /usr/bin/helium https://raw.githubusercontent.com/givpn/AutoScriptXray/master/helium/helium.sh
+chmod +x /usr/bin/helium
+helium
+
+fi
