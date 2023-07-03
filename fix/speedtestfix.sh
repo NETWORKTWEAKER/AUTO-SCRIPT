d43971232876cd619a351dc5fd6c56e0ea050f9c
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-07-03 20:23:41 +0700

    x

--- fix/speedtestfix.sh
+++ fix/speedtestfix.sh
@@ -3,16 +3,13 @@
 BGreen='\e[1;32m'
 NC='\e[0m'
 clear
-cd
 rm -rf /usr/bin/speedtest
 echo -e "\e[1;32m Start download speedtest.. \e[0m"
-cd /usr/bin
-wget -O speedtest "https://raw.githubusercontent.com/givpn/AutoScriptXray/master/fix/speedtest"
-chmod +x speedtest
+wget -q -O /usr/bin/speedtest https://raw.githubusercontent.com/givpn/AutoScriptXray/master/fix/speedtest.sh
+chmod +x /usr/bin/speedtest
 clear
 echo -e "\e[1;32m Setup done Please wait.. \e[0m"
 sleep 2
-cd
 rm -rf speedtestfix.sh
 clear
 echo -e "\e[1;32m auto reboot in 5s \e[0m"
