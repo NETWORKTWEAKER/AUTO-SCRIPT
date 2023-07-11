57140c27f99144aedce20ec3712498b459e2829a
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-07-11 19:11:31 +0700

    x

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# cari apa harta tahta hanya sementara ingat masih ada kehidupan setelah kematian
+# cari apa 
+# harta tahta hanya sementara ingat masih ada kehidupan setelah kematian
 # jangan lupa sholat
-# install 2x auto block
 clear
 red='\e[1;31m'
 green='\e[0;32m'
@@ -110,18 +110,6 @@ sleep 2
 
 mkdir -p /var/lib/ >/dev/null 2>&1
 echo "IP=" >> /var/lib/ipvps.conf
-if [ -f "/etc/xray/domain" ]; then
-echo ""
-echo -e "[ ${green}INFO${NC} ] Script Siap Diinstall"
-echo -ne "[ ${yell}WARNING${NC} ] Apakah Anda Ingin Mulai Menginstall? (y/n)? "
-read answer
-if [ "$answer" == "${answer#[Yy]}" ] ;then
-rm -rf setup.sh
-sleep 1
-exit 0
-else
-clear
-fi
 echo ""
 clear
 echo -e "$BBlue                           SETUP SUBDOMAIN VPS     $NC"
@@ -141,17 +129,14 @@ echo -e "$BYellow---------------------------------------------------------------
 #bash /root/cf | tee /root/install.log
 #print_success "Domain Random Done"
 #elif test $dns -eq 2; then
-read -rp "Enter Your Subdomain / Masukan Subdomain : " -e dom
-if [ -z $dom ]; then
-echo -e "Nothing input for domain.."
-else
+echo ""
+read -rp "Enter Your Subdomain / Masukan Subdomain : " dom
 echo "IP=$dom" > /var/lib/ipvps.conf
 echo "$dom" > /root/scdomain
 echo "$dom" > /etc/xray/scdomain
 echo "$dom" > /etc/xray/domain
 echo "$dom" > /etc/v2ray/domain
 echo $dom > /root/domain
-if
 echo -e "${BGreen}Done..${NC}"
 sleep 2
 clear
