48f59f2b88971fafe60a11500dde3df9286e75b7
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-07-11 22:30:55 +0700

    x

--- menu/m-sshovpn.sh
+++ menu/m-sshovpn.sh
@@ -18,6 +18,8 @@ echo -e " [\e[36m•8\e[0m] Set up Autokill SSH "
 echo -e " [\e[36m•9\e[0m] Cek Users Who Do Multi Login Multi"
 echo -e " [\e[36m•10\e[0m] User list created Account "
 echo -e " [\e[36m•11\e[0m] Change Banner SSH "
+echo -e " [\e[36m•12\e[0m] Lock User "
+echo -e " [\e[36m•13\e[0m] Unlock User "
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO MENU\033[0m"
 echo -e ""
@@ -39,6 +41,8 @@ case $opt in
 9) clear ; ceklim ; exit ;;
 10) clear ; cat /etc/log-create-ssh.log ; exit ;;
 11) clear ; nano /etc/issue.net ; exit ;;
+12) clear ; user-lock ; exit ;;
+13) clear ; user-unlock ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; m-sshovpn ;;
