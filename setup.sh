1a6fc2e05704fa9de0cbdd7cff35023859f1940d
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-07-11 09:33:16 +0700

    x

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# cari apa 
-# harta tahta hanya sementara ingat masih ada kehidupan setelah kematian
+# cari apa harta tahta hanya sementara ingat masih ada kehidupan setelah kematian
 # jangan lupa sholat
 # install 2x auto block
 clear
