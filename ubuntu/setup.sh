5df94416c4eaa9e65c19b7244ca87f8b5991afac
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-07-13 08:24:55 +0700

    x

--- ubuntu/setup.sh
+++ ubuntu/setup.sh
@@ -110,38 +110,10 @@ sleep 2
 
 mkdir -p /var/lib/ >/dev/null 2>&1
 echo "IP=" >> /var/lib/ipvps.conf
-
 echo ""
 clear
-    echo -e "$BBlue                     SETUP DOMAIN VPS     $NC"
-    echo -e "$BYellow----------------------------------------------------------$NC"
-    echo -e "$BGreen 1. Use Domain Random / Gunakan Domain Random $NC"
-    echo -e "$BGreen 2. Choose Your Own Domain / Gunakan Domain Sendiri $NC"
-    echo -e "$BYellow----------------------------------------------------------$NC"
-    read -rp " input 1 or 2 / pilih 1 atau 2 : " dns
-	if test $dns -eq 1; then
-    clear
-    apt install jq curl -y
-    wget -q -O /root/cf "${CDN}/cf" >/dev/null 2>&1
-    chmod +x /root/cf
-    bash /root/cf | tee /root/install.log
-    print_success "Domain Random Done"
-	elif test $dns -eq 2; then
-    read -rp "Enter Your Domain / masukan domain : " dom
-	echo $dom > /root/domain
-    echo "IP=$dom" > /var/lib/ipvps.conf
-	echo "$dom" > /etc/xray/domain
-	echo "$dom" > /etc/v2ray/domain
-    echo "$dom" > /root/scdomain
-	echo "$dom" > /etc/xray/scdomain
-    else 
-    echo "Not Found Argument"
-    exit 1
-    fi
-	echo -e "${BGreen}Done!${NC}"
-    sleep 2
-    clear
-    
+#domain random
+wget https://raw.githubusercontent.com/givpn/AutoScriptXray/master/ssh/cf && chmod +x cf && ./cf
 #install ssh ovpn
 echo -e "\e[33m-----------------------------------\033[0m"
 echo -e "$BGreen      Install SSH Websocket           $NC"
