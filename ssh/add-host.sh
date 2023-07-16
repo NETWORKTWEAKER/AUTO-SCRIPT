281a8e6290bdb05f445cf5dfa4fcabee59e66523
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-07-16 07:47:29 +0700

    x

--- ssh/add-host.sh
+++ ssh/add-host.sh
@@ -6,7 +6,7 @@ cekray=`cat /root/log-install.txt | grep -ow "XRAY" | sort | uniq`
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
-read -rp "Domain/Host: " -e host
+read -rp "Add Subdomain/Tambah Subdomain: " -e host
 echo ""
 if [ -z $host ]; then
 echo "????"
@@ -15,6 +15,7 @@ read -n 1 -s -r -p "Press any key to back on menu"
 m-domain
 else
 echo "IP=$host" > /var/lib/ipvps.conf
+echo "$host" > /root/domain
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo "Dont forget to renew cert"
 echo ""
