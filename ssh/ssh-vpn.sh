4a50965e5daa710853922bbc3fa1826410247111
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-07-29 20:52:25 +0700

    x

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -209,7 +209,7 @@ sed -i '/Port 22/a Port 40000' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 81' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 51443' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 58080' /etc/ssh/sshd_config
-sed -i '/Port 22/a Port 6666' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 666' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 200' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 22' /etc/ssh/sshd_config
 /etc/init.d/ssh restart
