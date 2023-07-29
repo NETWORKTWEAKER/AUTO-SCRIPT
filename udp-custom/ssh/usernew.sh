3cbfb7339c88ef766c1f6a2f0c1d2585a02ee1b6
Gilang Tok'yo <123780158+givpn@users.noreply.github.com>
2023-07-29 20:25:36 +0700

    x

--- udp-custom/ssh/usernew.sh
+++ udp-custom/ssh/usernew.sh
@@ -28,8 +28,8 @@ ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid Proxy" | cut -d: -f2)"
 ovpntcp="$(cat ~/log-install.txt | grep -w "OpenVPN TCP" | cut -d: -f2)"
 ovpnudp="$(cat ~/log-install.txt | grep -w "OpenVPN UDP" | cut -d: -f2)"
-tcp="1194"
-udp="2200"
+ovpntcp="1194"
+ovpnudp="2200"
 OhpSSH=`cat /root/log-install.txt | grep -w "OHP SSH" | cut -d: -f2 | awk '{print $1}'`
 OhpDB=`cat /root/log-install.txt | grep -w "OHP DBear" | cut -d: -f2 | awk '{print $1}'`
 OhpOVPN=`cat /root/log-install.txt | grep -w "OHP OpenVPN" | cut -d: -f2 | awk '{print $1}'`
@@ -71,8 +71,8 @@ echo -e "Port NS     : ALL Port (22, 443, 143)" | tee -a /etc/log-create-ssh.log
 echo -e "Nameserver  : $sldomain" | tee -a /etc/log-create-ssh.log
 echo -e "Pubkey      : $slkey" | tee -a /etc/log-create-ssh.log
 echo -e "Squid Proxy :$sqd" | tee -a /etc/log-create-ssh.log
-echo -e "OpenVPN TCP :$ovpntcp" | tee -a /etc/log-create-ssh.log
-echo -e "OpenVPN UDP :$ovpnudp" | tee -a /etc/log-create-ssh.log
+echo -e "OpenVPN TCP : $ovpntcp" | tee -a /etc/log-create-ssh.log
+echo -e "OpenVPN UDP : $ovpnudp" | tee -a /etc/log-create-ssh.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-ssh.log
 echo -e "\E[0;41;36m         INFO UDP Custom            \E[0m" | tee -a /etc/log-create-ssh.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-ssh.log
@@ -115,8 +115,8 @@ echo -e "Port NS     : ALL Port (22, 443, 143)" | tee -a /etc/log-create-ssh.log
 echo -e "Nameserver  : $sldomain" | tee -a /etc/log-create-ssh.log
 echo -e "Pubkey      : $slkey" | tee -a /etc/log-create-ssh.log
 echo -e "Squid Proxy :$sqd" | tee -a /etc/log-create-ssh.log
-echo -e "OpenVPN TCP :$ovpntcp" | tee -a /etc/log-create-ssh.log
-echo -e "OpenVPN UDP :$ovpnudp" | tee -a /etc/log-create-ssh.log
+echo -e "OpenVPN TCP : $ovpntcp" | tee -a /etc/log-create-ssh.log
+echo -e "OpenVPN UDP : $ovpnudp" | tee -a /etc/log-create-ssh.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-ssh.log
 echo -e "\E[0;41;36m         INFO UDP Custom            \E[0m" | tee -a /etc/log-create-ssh.log
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-ssh.log
