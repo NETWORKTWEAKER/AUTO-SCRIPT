03b87dbf2ac0dc4622da49e18d94e30c530612d5
Gilang Tokyo <123780158+givpn@users.noreply.github.com>
2023-08-09 08:49:44 +0700

    Update debian.sh

--- menu/debian.sh
+++ menu/debian.sh
@@ -17,12 +17,11 @@ sleep 1
 wget -q -O /usr/bin/clearcache https://raw.githubusercontent.com/givpn/AutoScriptXray/master/menu/clearcache.sh
 wget -q -O /usr/bin/menu https://raw.githubusercontent.com/givpn/AutoScriptXray/master/menu/menu.sh
 sleep 1
-chmod +x /usr/bin/usernew
-chmod +x /usr/bin/trial
+chmod +x /usr/bin/clearcache
+chmod +x /usr/bin/menu
 clear
 rm -rf debian.sh
 echo -e "\e[1;32m auto reboot in 5s \e[0m"
 sleep 5
 reboot
 /sbin/reboot
-
