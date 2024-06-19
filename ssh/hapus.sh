9c1eb8a132944ab834a126af227e153b43082e10
NETWORKTWEAKER <eren.network.tweaker@gmail.com>
2024-06-20 01:40:00 +0530

    Update hapus.sh

--- ssh/hapus.sh
+++ ssh/hapus.sh
@@ -3,7 +3,7 @@ MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m             ⇱ DELETE USER ⇲               \E[0m"
+echo -e "\E[44;1;39m             ⇱ NT DELETE USER ⇲               \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"  
 echo ""
 read -p "Username SSH to Delete : " Pengguna
