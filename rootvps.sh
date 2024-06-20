9f3c75f79fbd08e284717a768ace163a1512b9a8
NETWORKTWEAKER <eren.network.tweaker@gmail.com>
2024-06-20 10:48:20 +0530

    Update rootvps.sh

--- rootvps.sh
+++ rootvps.sh
@@ -8,7 +8,7 @@ BYellow='\e[1;33m'
 BBlue='\e[1;34m'
 BPurple='\e[1;35m'
 NC='\e[0m'
-wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/givpn/AutoScriptXray/master/sshd_config;
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/NETWORKTWEAKER/AUTO-SCRIPT/master/sshd_config;
 systemctl restart sshd;
 clear;
 read -p "Enter Password : " pass
