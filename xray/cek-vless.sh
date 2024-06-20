81100fcb80e243f3b2d98c24af16734f7ccfe661
NETWORKTWEAKER <eren.network.tweaker@gmail.com>
2024-06-20 10:30:14 +0530

    Update cek-vless.sh

--- xray/cek-vless.sh
+++ xray/cek-vless.sh
@@ -24,7 +24,7 @@ clear
 echo -n > /tmp/other.txt
 data=( `cat /etc/xray/config.json | grep '^####' | cut -d ' ' -f 2`);
 echo "----------------------------------------";
-echo "---------=[ Vless User Login ]=---------";
+echo "---------=[ NT Vless User Login ]=---------";
 echo "----------------------------------------";
 for akun in "${data[@]}"
 do
