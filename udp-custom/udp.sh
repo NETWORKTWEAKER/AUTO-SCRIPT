89c4d0dd127254dde55e5fbc8452c00d8c2f38fb
NETWORKTWEAKER <eren.network.tweaker@gmail.com>
2024-06-20 09:19:09 +0530

    Update udp.sh

--- udp-custom/udp.sh
+++ udp-custom/udp.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 # Script UdpCustom 2023
-# Script By MARDHEX
-# https://t.me/mdxdev
+# Script By NETWORK TWEAKER 
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition 1.0
-# Auther  : givpn
-# Kuota Habisss
+# Auther  : NETWORK TWEAKER 
+# EREN YEAGER AND CYBER NOVA 
 # (C) Copyright 2023
 # =========================================
 # pewarna hidup
@@ -23,13 +22,13 @@ rm -rf /usr/bin/trial
 rm -rf /root/domain
 echo "\e[1;32m Update Menu.. \e[0m"
 sleep 1
-wget -q -O /usr/bin/usernew https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/ssh/usernew.sh
-wget -q -O /usr/bin/trial https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/ssh/trial.sh
+wget -q -O /usr/bin/usernew https://raw.githubusercontent.com/NETWORKTWEAKER/AUTO-SCRIPT/master/udp-custom/ssh/usernew.sh
+wget -q -O /usr/bin/trial https://raw.githubusercontent.com/NETWORKTWEAKER/AUTO-SCRIPT/master/udp-custom/ssh/trial.sh
 echo "\e[1;32m Proses Download Script Slowdns.. \e[0m"
-wget https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
+wget https://raw.githubusercontent.com/NETWORKTWEAKER/AUTO-SCRIPT/master/udp-custom/slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
 sleep 1
 echo "\e[1;32m Proses Download Script OpenVPN.. \e[0m"
-wget https://raw.githubusercontent.com/givpn/AutoScriptXray/master/udp-custom/openvpn/openvpn.sh && chmod +x openvpn.sh && ./openvpn.sh
+wget https://raw.githubusercontent.com/NETWORKTWEAKER/AUTO-SCRIPT/master/udp-custom/openvpn/openvpn.sh && chmod +x openvpn.sh && ./openvpn.sh
 sleep 1
 chmod +x /usr/bin/usernew
 chmod +x /usr/bin/trial
@@ -50,7 +49,7 @@ clear
 echo "\e[1;32m Please Waiting Proses Downloading Toll UdpCustom.. \e[0m"
 sleep 1
 clear
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://github.com/givpn/AutoScriptXray/raw/master/udp-custom/udp-custom-linux-amd64" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://github.com/NETWORKTWEAKER/AUTO-SCRIPT/raw/master/udp-custom/udp-custom-linux-amd64" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
 chmod +x /root/udp/udp-custom
 clear
 # install Config Default Udp
@@ -68,13 +67,13 @@ clear
 echo "\e[1;32m Please Waiting Proses Downloading Default Config UdpCustom.. \e[0m"
 sleep 1
 clear
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://github.com/givpn/AutoScriptXray/raw/master/udp-custom/config.json" -O /root/udp/config.json && rm -rf /tmp/cookies.txt
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://github.com/NETWORKTWEAKER/AUTO-SCRIPT/raw/master/udp-custom/config.json" -O /root/udp/config.json && rm -rf /tmp/cookies.txt
 chmod 644 /root/udp/config.json
 
 if [ -z "$1" ]; then
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
-Description=UDP Custom by MARDHEX
+Description=UDP Custom by NETWORK TWEAKER 
 
 [Service]
 User=root
@@ -90,7 +89,7 @@ EOF
 else
 cat <<EOF > /etc/systemd/system/udp-custom.service
 [Unit]
-Description=UDP Custom by MARDHEX
+Description=UDP Custom by NETWORK TWEAKER 
 
 [Service]
 User=root
