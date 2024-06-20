c4ba46212166dbefa8cc34090e969c1310f5a214
NETWORKTWEAKER <eren.network.tweaker@gmail.com>
2024-06-20 08:41:22 +0530

    Update ins-helium.sh

--- helium/ins-helium.sh
+++ helium/ins-helium.sh
@@ -2,7 +2,7 @@
 # =========================================
 # Quick Setup | Script Setup Manager
 # Edition : Stable Edition 1.0
-# Auther  : givpn
+# Auther  : NETWORK TWEAKER
 # Kuota Habisss
 # (C) Copyright 2023
 # =========================================
@@ -12,15 +12,15 @@ NC='\e[0m'
 clear
 rm -rf /usr/bin/m-system
 echo -e "\e[1;32m Update Menu System.. \e[0m"
-wget -q -O /usr/bin/m-system https://raw.githubusercontent.com/givpn/AutoScriptXray/master/helium/menu/m-system.sh
+wget -q -O /usr/bin/m-system https://raw.githubusercontent.com/NETWORKTWEAKER/AUTO-SCRIPT/master/helium/menu/m-system.sh
 chmod +x /usr/bin/m-system
 rm -rf /usr/bin/wbmn
 echo -e "\e[1;32m Start download Panel Webmin.. \e[0m"
-wget -q -O /usr/bin/wbmn https://raw.githubusercontent.com/givpn/AutoScriptXray/master/helium/webmin/wbmn.sh
+wget -q -O /usr/bin/wbmn https://raw.githubusercontent.com/NETWORKTWEAKER/AUTO-SCRIPT/master/helium/webmin/wbmn.sh
 chmod +x /usr/bin/wbmn
 rm -rf /usr/bin/helium
 echo -e "\e[1;32m Start download Panel ADS Block.. \e[0m"
-wget -q -O /usr/bin/helium https://raw.githubusercontent.com/givpn/AutoScriptXray/master/helium/helium.sh
+wget -q -O /usr/bin/helium https://raw.githubusercontent.com/NETWORKTWEAKER/AUTO-SCRIPT/master/helium/helium.sh
 chmod +x /usr/bin/helium
 echo -e "\e[1;32m Setup done Please wait.. \e[0m"
 sleep 2
