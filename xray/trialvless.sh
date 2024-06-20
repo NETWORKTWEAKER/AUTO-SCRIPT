5fb59a2e005d7f75e6c3afba4ddd228bb8eaab00
NETWORKTWEAKER <eren.network.tweaker@gmail.com>
2024-06-20 10:45:07 +0530

    Update trialvless.sh

--- xray/trialvless.sh
+++ xray/trialvless.sh
@@ -15,7 +15,7 @@ vlesslink3="vless://${uuid}@${domain}:$tls?mode=gun&security=tls&encryption=none
 systemctl restart xray
 clear
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[44;1;39m        Trial Vless        \E[0m"
+echo -e "\E[44;1;39m        Trial NT Vless        \E[0m"
 echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "Remarks        : ${user}"
 echo -e "Domain         : ${domain}"
